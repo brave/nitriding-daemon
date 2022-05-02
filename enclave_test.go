0176e5aefeb1bc257a38a9b81ae92b2c30ba4899
Philipp Winter <phw@brave.com>
2022-05-01 20:56:59 -0700

    Add unit test for genSelfSignedCert.

---
+++ enclave_test.go
@@ -0,0 +1,25 @@
+package enclaveutils
+
+import (
+	"testing"
+)
+
+func createEnclave() *Enclave {
+	cfg := &Config{
+		SOCKSProxy: "socks5://127.0.0.1:1080",
+		FQDN:       "example.com",
+		Port:       50000,
+		UseACME:    false,
+		Debug:      false,
+		FdCur:      1024,
+		FdMax:      4096,
+	}
+	return NewEnclave(cfg)
+}
+
+func TestGenSelfSignedCert(t *testing.T) {
+	e := createEnclave()
+	if err := e.genSelfSignedCert(); err != nil {
+		t.Fatalf("Failed to create self-signed certificate: %s", err)
+	}
+}
