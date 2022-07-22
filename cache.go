68c5731df95b755c9fa58c055214d2e196b2fd24
Philipp Winter <phw@brave.com>
2022-07-22 08:48:33 -0500

    Revise cache pruning and add test.

--- cache.go
+++ cache.go
@@ -25,39 +25,43 @@ func newCache(ttl time.Duration) *cache {
 	}
 }
 
+// Count returns the number of elements in the cache.
+func (c *cache) Count() int {
+	c.RLock()
+	defer c.RUnlock()
+
+	return len(c.Items)
+}
+
+// pruneLater prunes the given element after ttl.
+func (c *cache) pruneLater(key string, ttl time.Duration) {
+	time.Sleep(ttl)
+
+	c.Lock()
+	defer c.Unlock()
+	delete(c.Items, key)
+}
+
 // Add adds a new string item to the cache.
 func (c *cache) Add(key string) {
 	c.Lock()
 	defer c.Unlock()
 
 	c.Items[key] = time.Now().UTC()
+	// Spawn a goroutine that deletes the given element after TTL.  Note that
+	// the enclave's endpoint for requesting nonces should not be exposed to
+	// the Internet because it would allow adversaries to request nonces at a
+	// high rate, thus spawning many goroutines, which would constitute a
+	// resource DoS attack.
+	go c.pruneLater(key, c.TTL)
 }
 
 // Exists returns true if the given string item exists in the cache.  If the
 // item exists but is expired, the function returns false.
 func (c *cache) Exists(key string) bool {
-	// Prune all expired cache items before checking if the given item exists.
-	// While that's inefficient, we don't care here because we use our cache
-	// only when enclaves synchronize their key material, which is rare.
-	c.prune()
-
 	c.RLock()
 	defer c.RUnlock()
 	_, exists := c.Items[key]
 
 	return exists
-}
-
-// prune removes expired items from the cache.
-func (c *cache) prune() {
-	c.Lock()
-	defer c.Unlock()
-
-	now := time.Now().UTC()
-	for item, expiryDate := range c.Items {
-		itemAge := now.Sub(expiryDate)
-		if itemAge > c.TTL {
-			delete(c.Items, item)
-		}
-	}
 }
--- cache_test.go
+++ cache_test.go
@@ -1,6 +1,7 @@
 package nitriding
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -23,5 +24,28 @@ func TestCache(t *testing.T) {
 	// Now ask for a non-existing item.
 	if c.Exists("bar") {
 		t.Errorf("Non-existing element is supposed to exist.")
+	}
+}
+
+func TestCacheWithManyElems(t *testing.T) {
+	c := newCache(time.Millisecond * 50)
+
+	// Add 100 items.
+	for i := 0; i < 100; i++ {
+		c.Add(fmt.Sprintf("%d", i))
+	}
+
+	// Wait for those 100 items to expire.
+	time.Sleep(time.Millisecond * 100)
+
+	// Add another 100 items.
+	for i := 100; i < 200; i++ {
+		c.Add(fmt.Sprintf("%d", i))
+	}
+
+	// We now expect 100 items to remain in the cache.
+	count := c.Count()
+	if count != 100 {
+		t.Fatalf("Expected 100 but got %d elems in cache.", count)
 	}
 }
