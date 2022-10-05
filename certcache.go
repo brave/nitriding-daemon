brave/nitriding-daemon

commit d21aa57ee065e11427dca6f9ac6ad0d7c969d9bb

Only use certificate cache outside an enclave.
We don't need a certificate cache when we're running inside an enclave
because when the enclave is gone, the certificate cache will be gone as
well.  Besides, the certificate cache makes it more difficult for the
enclave to switch to an unprivileged user before execution because we
now have to deal with the permissions of the cache directory and its
content.

This fixes https://github.com/brave/nitriding/issues/35.

enclave.go
@@ -310,10 +310,12 @@ func (e *Enclave) setupAcme() error {
 
 	elog.Printf("ACME hostname set to %s.", e.cfg.FQDN)
 	var cache autocert.Cache
-	if err = os.MkdirAll(acmeCertCacheDir, 0700); err != nil {
-		return fmt.Errorf("Failed to create cache directory: %w", err)
+	// Only use the cache directory when we're *not* in an enclave.  There's no
+	// point in writing certificates to disk when in an enclave because the
+	// disk does not persist when the enclave shuts down.
+	if !inEnclave {
+		cache = autocert.DirCache(acmeCertCacheDir)
 	}
-	cache = autocert.DirCache(acmeCertCacheDir)
 	certManager := autocert.Manager{
 		Cache:      cache,
 		Prompt:     autocert.AcceptTOS,

commit baae9e646d3da565739d6b624cf7d76982da704f

Add in-memory certificate cache.
This commit adds an in-memory certificate cache that we use by default.
This fixes a bug that I introduced in d21aa57.

certcache.go
@@ -1,0 +1,52 @@
+package nitriding
+
+import (
+	"context"
+	"errors"
+	"sync"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+var (
+	errHTTP01Failed = errors.New("failed to listen for HTTP-01 challenge")
+)
+
+// certCache implements the autocert.Cache interface.
+type certCache struct {
+	sync.RWMutex
+	cache map[string][]byte
+}
+
+func newCertCache() *certCache {
+	return &certCache{
+		cache: make(map[string][]byte),
+	}
+}
+
+func (c *certCache) Get(ctx context.Context, key string) ([]byte, error) {
+	c.RLock()
+	defer c.RUnlock()
+
+	cert, exists := c.cache[key]
+	if !exists {
+		return nil, autocert.ErrCacheMiss
+	}
+	return cert, nil
+}
+
+func (c *certCache) Put(ctx context.Context, key string, data []byte) error {
+	c.Lock()
+	defer c.Unlock()
+
+	c.cache[key] = data
+	return nil
+}
+
+func (c *certCache) Delete(ctx context.Context, key string) error {
+	c.Lock()
+	defer c.Unlock()
+
+	delete(c.cache, key)
+	return nil
+}

enclave.go
@@ -309,10 +309,13 @@ func (e *Enclave) setupAcme() error {
 	var err error
 
 	elog.Printf("ACME hostname set to %s.", e.cfg.FQDN)
-	var cache autocert.Cache
-	// Only use the cache directory when we're *not* in an enclave.  There's no
-	// point in writing certificates to disk when in an enclave because the
-	// disk does not persist when the enclave shuts down.
+	// By default, we use an in-memory certificate cache.  We only use the
+	// directory cache when we're *not* in an enclave.  There's no point in
+	// writing certificates to disk when in an enclave because the disk does
+	// not persist when the enclave shuts down.  Besides, dealing with file
+	// permissions makes it more complicated to switch to an unprivileged user
+	// ID before execution.
+	var cache autocert.Cache = newCertCache()
 	if !inEnclave {
 		cache = autocert.DirCache(acmeCertCacheDir)
 	}

certcache_test.go
@@ -1,0 +1,67 @@
+package nitriding
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"testing"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func TestGet(t *testing.T) {
+	var err error
+	var key = "foo"
+	var expectedCert = []byte("bar")
+	c := newCertCache()
+
+	// Retrieve non-existing key.
+	_, err = c.Get(context.TODO(), key)
+	if !errors.Is(err, autocert.ErrCacheMiss) {
+		t.Fatalf("Expected error %v but got %v.", autocert.ErrCacheMiss, err)
+	}
+
+	// Retrieve existing key.
+	_ = c.Put(context.TODO(), key, expectedCert)
+	cert, err := c.Get(context.TODO(), key)
+	if err != nil {
+		t.Fatalf("Expected no error but got %v.", err)
+	}
+	if !bytes.Equal(expectedCert, cert) {
+		t.Fatalf("Expected value %s but got %s.", string(expectedCert), string(cert))
+	}
+}
+
+func TestPut(t *testing.T) {
+	var err error
+	var key = "foo"
+	var expectedCert = []byte("bar")
+	c := newCertCache()
+
+	if err = c.Put(context.TODO(), key, expectedCert); err != nil {
+		t.Fatalf("Expected no error but got %v.", err)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	var key = "foo"
+	var err error
+	c := newCertCache()
+
+	_ = c.Put(context.TODO(), key, []byte("bar"))
+	if err = c.Delete(context.TODO(), key); err != nil {
+		t.Fatalf("Expected no error but got %v.", err)
+	}
+	if len(c.cache) != 0 {
+		t.Fatal("Expected cache to be empty but it's not.")
+	}
+
+	// Try deleting the same element again.  This should not result in an error
+	// as our Delete never returns an error.
+	if err = c.Delete(context.TODO(), key); err != nil {
+		t.Fatalf("Expected no error but got %v.", err)
+	}
+	if len(c.cache) != 0 {
+		t.Fatal("Expected cache to be empty but it's not.")
+	}
+}