3ffa59d6c9a12469fd760b4a6cb5c002c9af1d5e
Philipp Winter <phw@brave.com>
2022-10-24 11:34:28 -0500

    Add Makefile and refactor standalone application.

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,44 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 
 	"github.com/brave/nitriding"
 )
 
+var l = log.New(os.Stderr, "nitriding-cmd: ", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
+
 func main() {
-	log.Printf("Running as UID %d.", os.Getuid())
+	var sockAddr, fqdn, appURL string
+	var port, hostProxyPort int
+	var useACME bool
+
+	flag.StringVar(&fqdn, "fqdn", "example.com", "FQDN for the enclave application.")
+	flag.StringVar(&sockAddr, "sockaddr", "/tmp/nitriding.sock", "Path to unix domain socket for enclave-internal IPC.")
+	flag.StringVar(&appURL, "appurl", "github.com/foo/bar", "Code repository for the enclave application.")
+	flag.IntVar(&port, "port", 8443, "Nitriding's VSOCK-facing HTTP port.  Must match port forwarding rules on EC2 host.")
+	flag.IntVar(&hostProxyPort, "host-proxy-port", 1024, "Port of proxy application running on EC2 host.")
+	flag.BoolVar(&useACME, "acme", true, "Use Let's Encrypt's ACME to fetch HTTPS certificate.")
+	flag.Parse()
 
 	enclave, err := nitriding.NewEnclave(
 		&nitriding.Config{
-			FQDN:    "nitro.nymity.ch",
-			Port:    8080,
-			UseACME: false,
-			Debug:   true,
+			FQDN:          fqdn,
+			Port:          port,
+			HostProxyPort: hostProxyPort,
+			UseACME:       useACME,
+			SockAddr:      sockAddr,
+			AppURL:        appURL,
 		},
 	)
 	if err != nil {
-		log.Fatal(err)
+		l.Fatalf("Failed to create enclave: %v", err)
 	}
 
 	// Start blocks for as long as the enclave is alive.
 	if err := enclave.Start(); err != nil {
-		log.Fatalf("Enclave terminated: %v", err)
+		l.Fatalf("Enclave terminated: %v", err)
 	}
 }
