2a8f019d534937449295f0c4107611b0e7cdfc7a
Philipp Winter <phw@brave.com>
2023-09-10 21:38:01 -0500

    Add unit test.

--- attester_test.go
+++ attester_test.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"testing"
 
 	"github.com/hf/nitrite"
 )
+
+func TestDummyAttestation(t *testing.T) {
+	var (
+		d               = newDummyAttester()
+		workersNonce    = nonce{1, 2, 3}
+		hashOfEncrypted = []byte("this is a hash")
+	)
+
+	attstn, err := d.createAttstn(&leaderAuxInfo{
+		WorkersNonce:    workersNonce,
+		HashOfEncrypted: hashOfEncrypted,
+	})
+	failOnErr(t, err)
+
+	aux, err := d.verifyAttstn(attstn, workersNonce)
+	failOnErr(t, err)
+
+	leaderAux := aux.(*leaderAuxInfo)
+	if leaderAux.WorkersNonce != workersNonce {
+		t.Fatal("Extracted unexpected workers nonce.")
+	}
+	if !bytes.Equal(leaderAux.HashOfEncrypted, hashOfEncrypted) {
+		t.Fatalf("Extracted unexpected hash over encrypted keys.")
+	}
+}
 
 func TestVerifyNitroAttstn(t *testing.T) {
 	var n = newNitroAttester()
